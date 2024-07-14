d16076704d8ee6c8a60fccc33e34d1f92b1a976e
Kung <sales@caryelectronic.com>
2024-07-15 00:48:25 +0800

    some

--- r68s/6.6/diy-part2.sh
+++ r68s/6.6/diy-part2.sh
@@ -34,12 +34,13 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-th
 
 # 增减常用软件包: luci-app-smartdns换为需要操作的包名，增=y， 减=n
 # sed -i "s/\(luci-app-smartdns\)=y/\1=n/" .config
-sed -i "s/\(luci-app-bypass\)=y/\1=n/" .config
-sed -i "s/\(luci-app-passwall\)=y/\1=n/" .config
-sed -i "s/\(luci-app-ssr-plus\)=y/\1=n/" .config
+sed -i 's/\(luci-app-bypass\)=y/\1=n/' .config
+sed -i 's/\(luci-app-passwall\)=y/\1=n/' .config
+sed -i 's/\(luci-app-ssr-plus\)=y/\1=n/' .config
 
 # 添加额外软件包
-sed -i "CONFIG_PACKAGE_luci-app-homeproxy=y" .config
+sed -i '$a CONFIG_PACKAGE_luci-app-homeproxy=y' .config
+
 
 # 科学上网插件
 
