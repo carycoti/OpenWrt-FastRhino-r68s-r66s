936e1b9105e04dabfdbee1cb06d8d305c0110daa
Kung <30816864+carycoti@users.noreply.github.com>
2024-07-15 04:47:04 +0800

    Update diy-part1.sh

--- r68s/6.6/diy-part1.sh
+++ r68s/6.6/diy-part1.sh
@@ -18,14 +18,24 @@ shopt -s extglob
 # 添加软件源
 # sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
 
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ 
 sed -i "/telephony/d" feeds.conf.default
-
 sed -i "s?targets/%S/packages?targets/%S/\$(LINUX_VERSION)?" include/feeds.mk
-
 sed -i '/	refresh_config();/d' scripts/feeds
 
 ./scripts/feeds update -a
-# ./scripts/feeds install -a -p kiddin9 -f
+#./scripts/feeds install -a -p kiddin9 -f
+
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
 ./scripts/feeds install -a
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
