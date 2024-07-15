1c222f5409c13353450c058859a7d28fd000dcd5
Kung <sales@caryelectronic.com>
2024-07-16 02:25:02 +0800

    some

--- r68s/6.6/diy-part2.sh
+++ r68s/6.6/diy-part2.sh
@@ -31,6 +31,8 @@ rm -rf feeds/luci/themes/luci-theme-argon
 
 # Themes
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 # 添加额外软件包
 echo 'CONFIG_PACKAGE_luci-app-diskman=y' >>.config
