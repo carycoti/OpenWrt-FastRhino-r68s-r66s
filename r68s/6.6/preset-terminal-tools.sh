cdd29a6bd4753209027dc49b7467a0563d03af0e
Kung <sales@caryelectronic.com>
2024-07-20 23:24:35 +0800

    some

--- r68s/6.6/preset-terminal-tools.sh
+++ r68s/6.6/preset-terminal-tools.sh
@@ -5,5 +5,6 @@ mkdir -p files
 pushd files
 git clone --depth=1 https://github.com/jaivardhankapoor/bestbash root/.bash
 # rm -f etc/bash.bashrc
+mkdir etc
 ln -s root/.bash/init etc/bash.bashrc
 popd
