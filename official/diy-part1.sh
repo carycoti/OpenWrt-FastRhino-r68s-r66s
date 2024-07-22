2aba91254404fddd171de2165f31c61ad383b9eb
Kung <sales@caryelectronic.com>
2024-07-23 04:58:51 +0800

    some

---
+++ official/diy-part1.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# =================================================================
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# =================================================================
+
+# 执行命令来切换内核
+#sed -i 's/PATCHVER:=6.1/PATCHVER:=6.6/g' target/linux/rockchip/Makefile
+
+# 添加软件源
+shopt -s extglob
+# 添加软件源
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
+
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ 
+sed -i "/telephony/d" feeds.conf.default
+sed -i "s?targets/%S/packages?targets/%S/\$(LINUX_VERSION)?" include/feeds.mk
+sed -i '/	refresh_config();/d' scripts/feeds
+
+./scripts/feeds update -a
+./scripts/feeds install -a -p kiddin9 -f
+
+# 修改 Makefile
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
+
+# 取消对 samba4 的菜单调整
+sed -i '/samba4/s/^/#/' package/lean/default-settings/files/zzz-default-settings
+
+# Themes
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-bootstrap-mmdvm
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+# rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+# rm -rf feeds/packages/utils/v2dat
+# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+rm -f feeds/packages/lang/ruby
+git clone https://github.com/coolsnowwolf/packages tmp/lede-packages
+mv tmp/lede-packages/lang/ruby feeds/packages/lang/ruby
+
+./scripts/feeds install -a
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/Jason6111/helloworld' >>feeds.conf.default
+
+# 添加第三方软件包
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
---
+++ official/diy-part2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# ==============================================================
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# ===============================================================
+
+#修改默认IP
+sed -i 's/192.168.1.1/192.168.1.21/g' package/base-files/files/bin/config_generate   # 定制默认IP
+# 默认网关和dns
+sed -i 's/\(\([ \t]\+\)set network.$1.netmask=.*\)/\1\n \2set network.$1.dns='192.168.1.1'/' package/base-files/files/bin/config_generate
+sed -i 's/\(\([ \t]\+\)set network.$1.netmask=.*\)/\1\n \2set network.$1.gateway='192.168.1.1'/' package/base-files/files/bin/config_generate
+
+
+# Configure pppoe connection
+#uci set network.wan.proto=pppoe
+#uci set network.wan.username='yougotthisfromyour@isp.su'
+#uci set network.wan.password='yourpassword'
+
+# 修改软件包
+# chmod -R 755 files
+# rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/{alist,adguardhome,smartdns}
+# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# 添加额外软件包
+echo 'CONFIG_PACKAGE_luci-app-diskman=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-samba4=y' >>.config
+echo 'CONFIG_PACKAGE_docker-compose=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-istorex=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-linkease=y' >>.config
+
+# 科学上网插件
+
+
+# 科学上网插件依赖
