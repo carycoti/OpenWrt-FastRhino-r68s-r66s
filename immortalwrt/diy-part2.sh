8e8b267217541dfcfaac09b72583d4384678f97c
Kung <sales@caryelectronic.com>
2024-07-26 17:22:54 +0800

    some

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -22,7 +22,7 @@ sed -i 's/\(\([ \t]\+\)set network.$1.netmask=.*\)/\1\n \2set network.$1.gateway
 # 更改默认 Shell 为 bash
 # sed -i 's/\/bin\/ash/\/bin\/bash/g' package/base-files/files/etc/passwd
 # 改为 fish
-sed -i "s/\/bin\/bash/\/usr\/bin\/fish/" package/base-files/files/etc/passwd
+sed -i "s/\/bin\/ash/\/usr\/bin\/fish/" package/base-files/files/etc/passwd
 
 # 更改firewall设置：常规设置第3行和第5行的入站和转发设为接受
 sed -i "3s/REJECT/ACCEPT/" package/network/config/firewall/files/firewall.config
