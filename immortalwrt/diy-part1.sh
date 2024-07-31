69d406355f82b1306df06a795a5b15af8fe558c6
Kung <sales@caryelectronic.com>
2024-07-31 17:45:17 +0800

    some

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -16,12 +16,12 @@
 # 添加软件源
 shopt -s extglob
 # 添加软件源
-# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
 
 # kenzok8
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 # istore
 # echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
@@ -41,52 +41,45 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 # sed -i '/samba4/s/^/#/' package/lean/default-settings/files/zzz-default-settings
 
 ./scripts/feeds update -a
-# ./scripts/feeds install -a -p kiddin9 -f
 
 # 替换中文,解决中文包无效的问题
 # sed -i "s/option lang auto/option lang zh_cn/" feeds/luci/modules/luci-base/root/etc/config/luci
 # sed -i "/config internal languages/a\\\toption zh_cn 'chinese'\n\toption en 'English'" feeds/luci/modules/luci-base/root/etc/config/luci
 
 # Themes
-rm -rf feeds/luci/themes/luci-theme-argon
-#rm -rf feeds/luci/themes/luci-theme-bootstrap-mmdvm
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# rm -rf feeds/luci/themes/luci-theme-argon
+# #rm -rf feeds/luci/themes/luci-theme-bootstrap-mmdvm
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 # kenzok8
-rm -rf feeds/kenzo/luci-theme-argon
-# rm -rf feeds/kenzo/luci-app-istorex
-# rm -rf feeds/kenzo/luci-app-quickstart
-# rm -rf feeds/kenzo/quickstart
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+# rm -rf feeds/kenzo/luci-theme-argon
+# # rm -rf feeds/kenzo/luci-app-istorex
+# # rm -rf feeds/kenzo/luci-app-quickstart
+# # rm -rf feeds/kenzo/quickstart
+# rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+# rm -rf feeds/packages/utils/v2dat
+# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-rm -rf feeds/packages/lang/ruby
-git clone https://github.com/coolsnowwolf/packages tmp/lede-packages
-mv tmp/lede-packages/lang/ruby feeds/packages/lang/ruby
-mv tmp/lede-packages/net/gowebdav feeds/packages/net/gowebdav
-mv tmp/lede-packages/utils/bandwidthd feeds/packages/utils/bandwidthd
-mv tmp/lede-packages/net/shadowsocks-libev feeds/packages/net/shadowsocks-libev
-mv tmp/lede-packages/libs/pcre2 feeds/packages/libs/pcre2
-rm -rf tmp/lede-packages
+# rm -rf feeds/packages/lang/ruby
+# git clone https://github.com/coolsnowwolf/packages tmp/lede-packages
+# mv tmp/lede-packages/lang/ruby feeds/packages/lang/ruby
+# mv tmp/lede-packages/net/gowebdav feeds/packages/net/gowebdav
+# mv tmp/lede-packages/utils/bandwidthd feeds/packages/utils/bandwidthd
+# mv tmp/lede-packages/net/shadowsocks-libev feeds/packages/net/shadowsocks-libev
+# mv tmp/lede-packages/libs/pcre2 feeds/packages/libs/pcre2
+# rm -rf tmp/lede-packages
 # mv tmp/lede-packages/lang/lua-maxminddb feeds/packages/lang/lua-maxminddb
 # mv tmp/lede-packages/net/redsocks2 feeds/packages/net/redsocks2
 # mv tmp/lede-packages/net/trojan-plus feeds/packages/net/trojan-plus
 
 # 2024-07-24临时更改 HASH
 sed -i "s/PKG_MIRROR_HASH:=ed7c71afb74c8232cfda084545ad8c1f8fe6c5e8176da9a77729e1fa9044d863/PKG_MIRROR_HASH:=skip/" feeds/packages/net/trojan-plus/Makefile
-
 sed -i "s/PKG_MIRROR_HASH:=0981bf49cb8a6e7f81912808987727bf6a2454ad0a3cf744f1a64bfc1969b088/PKG_MIRROR_HASH:=skip/" feeds/packages/net/redsocks2/Makefile
-
-sed -i "s/PKG_MIRROR_HASH:=0981bf49cb8a6e7f81912808987727bf6a2454ad0a3cf744f1a64bfc1969b088/PKG_MIRROR_HASH:=skip/" feeds/small/redsocks2/Makefile
-
 sed -i "s/PKG_MIRROR_HASH:=e70dd8843c3688b58f66fff5320a93d5789b79114bcb36a94d5b554664439f04/PKG_MIRROR_HASH:=skip/" feeds/packages/lang/lua-maxminddb/Makefile
-
-sed -i "s/PKG_MIRROR_HASH:=e70dd8843c3688b58f66fff5320a93d5789b79114bcb36a94d5b554664439f04/PKG_MIRROR_HASH:=skip/" feeds/kenzo/lua-maxminddb/Makefile
 
 # 添加第三方软件包
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
@@ -100,5 +93,6 @@ sed -i "s/PKG_MIRROR_HASH:=e70dd8843c3688b58f66fff5320a93d5789b79114bcb36a94d5b5
 
 # ./scripts/feeds install -a -p nas -f
 # ./scripts/feeds install -a -p nas_luci -f
+./scripts/feeds install -a -p kiddin9 -f
 ./scripts/feeds install -a
 # mv -f feeds/kiddin9/r81* tmp/
