58f86716e525a63a7c4a3c147db4462db0bc31ee
Kung <sales@caryelectronic.com>
2024-08-08 11:31:47 +0800

    some

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -59,8 +59,8 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/{v2dat,gl-mifi-mcu}
-rm -rf feeds/packages/libs/{dmx_usb_module}
-rm -rf package/lean/{r8101}
+rm -rf feeds/packages/libs/dmx_usb_module
+rm -rf package/lean/r8101
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
 rm -rf feeds/packages/lang/golang
