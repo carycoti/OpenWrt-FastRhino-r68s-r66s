44e7f3e866e8ae3b711f72fc9b7b4b0b882fb7d4
Kung <sales@caryelectronic.com>
2024-08-10 13:14:25 +0800

    some

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -63,6 +63,7 @@ rm -rf feeds/packages/libs/{dmx_usb_module,libpfring}
 rm -rf package/lean/r8101
 rm -rf package/wwan/driver/quectel_SRPD_PCIE
 rm -rf package/kernel/{ath10k-ct,ubnt-ledbar,mac80211}
+rm -rf feeds/routing/batman-adv
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
 rm -rf feeds/packages/lang/golang
